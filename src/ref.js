5946b5b7d1c29aa70ba21f01c99c0b7d6d3cbda1
jwhgzs <admin@jwhgzs.com>
2022-10-22 22:33:53 +0800

    v0.5.0

--- src/ref.js
+++ src/ref.js
@@ -26,7 +26,8 @@ export default class Ref {
             __rawValue: obj,
             __realValue: nobj,
             __deps: deps,
-            __depTag: depTag
+            __depTag: depTag,
+            __traffic: null
         }, {
             get(target, key) {
                 if (key == '__getRaw')
@@ -34,7 +35,17 @@ export default class Ref {
                 if (key == '__setRaw') {
                     return (k, v) => {
                         target.__rawValue[k] = v
-                        target.__realValue[k] = Ref.createRef(_this, v, deps, depTag)
+                        target.__realValue[k] = Ref.createRef(_this, v, target.__deps, target.__depTag)
+                    }
+                }
+                if (key == '__setTraffic') {
+                    return (val) => {
+                        target.__traffic = val
+                    }
+                }
+                if (key == '__notify') {
+                    return () => {
+                        target.__deps.notify(target.__depTag)
                     }
                 }
                 
@@ -55,15 +66,18 @@ export default class Ref {
             },
             set(target, key, value) {
                 let changed = value !== target.__rawValue[key]
-                target.__rawValue[key] = value
-                target.__realValue[key] = Ref.createRef(_this, value, deps, depTag)
-                if (changed)
-                    target.__deps.notify(target.__depTag || key)
+                if (changed) {
+                    target.__rawValue[key] = value
+                    target.__realValue[key] = Ref.createRef(_this, value, deps, depTag)
+                    if (! target.__traffic)
+                        target.__deps.notify(target.__depTag || key)
+                }
                 return true
             },
             deleteProperty(target, key) {
                 delete target.__rawValue[key], target.__realValue[key]
-                target.__deps.notify(target.__depTag || key)
+                if (! target.__traffic)
+                    target.__deps.notify(target.__depTag || key)
                 return true
             }
         })
@@ -74,4 +88,120 @@ export default class Ref {
     static isArrayRef(obj) {
         return this.isRef(obj) && obj.__getRaw('__isArray')
     }
+}
+
+Array.prototype.push = function (...items) {
+    if (Ref.isRef(this))
+        this.__setTraffic('push')
+    
+    for (let i = 0; i < items.length; i ++) {
+        this[this.length] = items[i]
+    }
+    
+    if (Ref.isRef(this)) {
+        this.__setTraffic(null)
+        this.__notify()
+    }
+    return items.length
+}
+Array.prototype.unshift = function (...items) {
+    if (Ref.isRef(this))
+        this.__setTraffic('unshift')
+    
+    for (let k = this.length + items.length - 1; k >= 0; k --) {
+        if (k >= items.length)
+            this[k] = this[k - items.length]
+        else
+            this[k] = items[k]
+    }
+    
+    if (Ref.isRef(this)) {
+        this.__setTraffic(null)
+        this.__notify()
+    }
+    return items.length
+}
+Array.prototype.reverse = function () {
+    if (Ref.isRef(this))
+        this.__setTraffic('reverse')
+    
+    for (let k = 0; k < Math.floor(this.length / 2); k ++) {
+        let ek = this.length - 1 - k
+        let a = this[k], b = this[ek]
+        this[k] = b
+        this[ek] = a
+    }
+    
+    if (Ref.isRef(this)) {
+        this.__setTraffic(null)
+        this.__notify()
+    }
+    return this
+}
+Array.prototype.sort = function (func = (a, b) => {
+    a = parseInt(('' + a).substr(0, 1))
+    b = parseInt(('' + b).substr(0, 1))
+    return a > b
+}) {
+    if (Ref.isRef(this))
+        this.__setTraffic('sort')
+    
+    for (let k = 0; k < this.length; k ++) {
+        for (let k2 = k + 1; k2 < this.length; k2 ++) {
+            let a = this[k], b = this[k2]
+            if (func(a, b)) {
+                this[k] = b
+                this[k2] = a
+            }
+        }
+    }
+    
+    if (Ref.isRef(this)) {
+        this.__setTraffic(null)
+        this.__notify()
+    }
+    return this
+}
+Array.prototype.splice = function (sk = 0, count = 1, ...items) {
+    if (Ref.isRef(this))
+        this.__setTraffic('splice')
+    
+    if (sk < 0)
+        sk = this.length + sk
+    if (count < 0)
+        count = 0
+    
+    let bin = [], diff = items.length - count,
+        length = this.length + diff
+    let doit = (k, order) => {
+        if (k >= sk && k < sk + count) {
+            if (order == 1)
+                bin.push(this[k])
+            else
+                bin.unshift(this[k])
+        }
+        
+        if (k - sk <= items.length - 1)
+            this[k] = items[k - sk]
+        else
+            this[k] = this[k - diff]
+    }
+    if (diff >= 0) {
+        for (let k = length - 1; k >= sk; k --)
+            doit(k, 2)
+    } else {
+        for (let k = sk; k < length; k ++)
+            doit(k, 1)
+    }
+    if (length < this.length) {
+        for (let k = length; k < this.length; k ++)
+            bin.push(this[k])
+        this.length = length
+    }
+    
+    if (Ref.isRef(this)) {
+        this.__setTraffic(null)
+        this.__notify()
+    }
+    return bin
 }
